9eecacd28d726a6bcb0b4090a496b979d20a6ea0
yatharth <yatharth.saini.int@paymeindia.in>
2023-11-08 10:43:27 +0530

    UPDATED: Email helper and utils.py

--- cash_flow/api/v1/utils.py
+++ cash_flow/api/v1/utils.py
@@ -1,19 +1,47 @@
 from django.core.mail import send_mail, EmailMessage
 from django.conf import settings
 
+from enum import Enum
 
-class Util:
+
+class MethodNames(Enum):
+    """
+    Enum For Method Names
+    """
+
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+    DELETE = "DELETE"
+    PATCH = "PATCH"
+    OPTIONS = "OPTIONS"
+    HEAD = "HEAD"
+
+
+class EmailHelper:
     @staticmethod
-    def send_email(data, file):
-        try:
-            subject = data['subject']
-            body = data['body']
+    def send_mail(subject, message, recipient_list, attachments=None, from_email=None):
+        if from_email is None:
             from_email = settings.DEFAULT_FROM_EMAIL
-            to_email = data['to_email']
 
-            email = EmailMessage(subject, body, from_email, to_email)
-            email.attach_file(file)
-            email.send()
-            return True
-        except Exception as e:
-            return False
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=from_email,
+            recipient_list=recipient_list,
+            fail_silently=False
+        )
+
+    @staticmethod
+    def send_email_with_attachment(subject, message, recipient_list, attachments=None, from_email=None):
+        if from_email is None:
+            from_email = settings.DEFAULT_FROM_EMAIL
+
+        email = EmailMessage(
+            subject=subject,
+            body=message,
+            from_email=from_email,
+            to=recipient_list,
+            attachments=attachments if attachments else [],
+        )
+        email.send()
