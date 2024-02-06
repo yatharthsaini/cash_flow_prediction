a542883c01e9ef1ac4a84ec98d2d851f9e7f7129
yatharth <yatharth.saini.int@paymeindia.in>
2024-02-06 10:58:45 +0530

    UPDATED: cash_flow.admin.py

--- cash_flow/admin.py
+++ cash_flow/admin.py
@@ -4,16 +4,22 @@ from cash_flow.models import NBFCEligibilityCashFlowHead, CollectionAndLoanBooke
 # Register your models here.
 admin.site.register(NBFCEligibilityCashFlowHead)
 
+
 class CollectionAndLoanBookedDataAdmin(admin.ModelAdmin):
     model = CollectionAndLoanBookedData
     search_fields = ['nbfc']
     list_display = ['nbfc', 'due_date', 'collection']
     list_filter = ['nbfc', 'due_date']
+
+
 admin.site.register(CollectionAndLoanBookedData, CollectionAndLoanBookedDataAdmin)
+
 
 class LoanDetailAdmin(admin.ModelAdmin):
     model = LoanDetail
     search_fields = ['nbfc', 'user_id', 'status', 'credit_limit', 'user_type']
     list_display = ['nbfc', 'user_id', 'credit_limit', 'status', 'user_type']
     list_filter = ['nbfc', 'user_id', 'status', 'credit_limit', 'user_type']
+
+
 admin.site.register(LoanDetail, LoanDetailAdmin)
