2d667bb0e6c71e759ad41ff9302264fb70fecc9c
vimal <vimal.mahawar@paymeindia.in>
2024-02-06 09:24:58 +0530

    ADDED : list search

--- cash_flow/admin.py
+++ cash_flow/admin.py
@@ -6,10 +6,14 @@ admin.site.register(NBFCEligibilityCashFlowHead)
 
 class CollectionAndLoanBookedDataAdmin(admin.ModelAdmin):
     model = CollectionAndLoanBookedData
+    search_fields = ['nbfc']
     list_display = ['nbfc', 'due_date', 'collection']
+    list_filter = ['nbfc', 'due_date']
 admin.site.register(CollectionAndLoanBookedData, CollectionAndLoanBookedDataAdmin)
 
 class LoanDetailAdmin(admin.ModelAdmin):
     model = LoanDetail
-    list_display = ['nbfc', 'due_date', 'collection']
+    search_fields = ['nbfc', 'user_id', 'status', 'credit_limit', 'user_type']
+    list_display = ['nbfc', 'user_id', 'credit_limit', 'status', 'user_type']
+    list_filter = ['nbfc', 'user_id', 'status', 'credit_limit', 'user_type']
 admin.site.register(LoanDetail, LoanDetailAdmin)
