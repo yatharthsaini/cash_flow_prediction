97ac2e883c4d8a5515a140938b86790f7b22dfab
yatharth <yatharth.saini.int@paymeindia.in>
2024-03-05 11:22:52 +0530

    UPDATED: signals code for should_check and should_assign

--- cash_flow/signals.py
+++ cash_flow/signals.py
@@ -6,28 +6,17 @@ from cash_flow.models import NBFCEligibilityCashFlowHead
 
 
 @receiver(post_save, sender=NBFCEligibilityCashFlowHead)
-def create_should_check_cache(sender, instance, **kwargs):
+def create_should_check_and_should_assign(sender, instance, **kwargs):
     """
-    signal function to create cache for should check attribute
+    signal function to create cache for should check and should assign attribute and cache time =~ 10 years
     :return:
     """
     try:
         should_check_branches = set(
             NBFCEligibilityCashFlowHead.objects.filter(should_check=True).values_list('nbfc_id', flat=True))
-        cache.set('should_check', list(should_check_branches), timeout=None)
-    except Exception as e:
-        print(e)
-
-
-@receiver(post_save, sender=NBFCEligibilityCashFlowHead)
-def create_should_assign_cache(sender, instance, **kwargs):
-    """
-    signal function to create cache for should assign attribute
-    :return:
-    """
-    try:
+        cache.set('should_check', list(should_check_branches), timeout=315360000)
         should_assign_branches = set(
             NBFCEligibilityCashFlowHead.objects.filter(should_assign=True).values_list('nbfc_id', flat=True))
-        cache.set('should_check', list(should_assign_branches), timeout=None)
+        cache.set('should_assign', list(should_assign_branches), timeout=315360000)
     except Exception as e:
         print(e)
