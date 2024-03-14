48ce655c86ca790fdd0dc9d57ab61a51124d095f
yatharth <yatharth.saini.int@paymeindia.in>
2024-03-14 17:11:43 +0530

    UPDATED: timezone w.r.t paymeindia server

--- cash_flow_prediction/settings.py
+++ cash_flow_prediction/settings.py
@@ -208,13 +208,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_L10N = True
+
+USE_TZ = False
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
